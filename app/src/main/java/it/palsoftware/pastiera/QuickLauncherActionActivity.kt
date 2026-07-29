7ad8559125c18e6c12625c269e4ca8bb796fc506
astroboii47 <aaron.kim77@gmail.com>
2026-07-29 16:49:36 +1000

    Add legacy QuickLauncher shortcut provider

--- app/src/main/java/it/palsoftware/pastiera/QuickLauncherActionActivity.kt
+++ app/src/main/java/it/palsoftware/pastiera/QuickLauncherActionActivity.kt
@@ -1,6 +1,7 @@
 package it.palsoftware.pastiera
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import it.palsoftware.pastiera.inputmethod.QuickLauncherOpener
 
@@ -19,10 +20,23 @@ class QuickLauncherActionActivity : Activity() {
     }
 
     private fun handleIntent() {
-        if (intent?.action != ACTION_OPEN_QUICK_LAUNCHER) {
-            return
+        when (intent?.action) {
+            Intent.ACTION_CREATE_SHORTCUT -> returnShortcut()
+            ACTION_OPEN_QUICK_LAUNCHER -> QuickLauncherOpener.open(this)
         }
-        QuickLauncherOpener.open(this)
+    }
+
+    private fun returnShortcut() {
+        val shortcutIntent = Intent(ACTION_OPEN_QUICK_LAUNCHER)
+            .setClass(this, QuickLauncherActionActivity::class.java)
+        val result = Intent()
+            .putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent)
+            .putExtra(Intent.EXTRA_SHORTCUT_NAME, getString(R.string.quick_launcher_shortcut_short))
+            .putExtra(
+                Intent.EXTRA_SHORTCUT_ICON_RESOURCE,
+                Intent.ShortcutIconResource.fromContext(this, R.mipmap.ic_launcher)
+            )
+        setResult(RESULT_OK, result)
     }
 
     companion object {
